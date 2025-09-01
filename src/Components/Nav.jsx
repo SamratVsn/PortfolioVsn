73c0ee474b19e7949f575b33e07196f3d26a231c
SamratVsn1 <code.samzt13@gmail.com>
2025-09-01 08:11:20 +0545

    Nav Updates
    Navigation Bar updated for mobile responsiveness

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -104,16 +104,15 @@ const Navbar = () => {
 
       {/* Mobile Dropdown */}
       {isOpen && (
-        <div className="md:hidden bg-[#0441e7] border border-cyan-500/40">
-          <ul className="flex flex-col px-6 py-4 space-y-3 text-base font-medium">
+        <div className="md:hidden bg-gradient-to-b from-[#0a26dd] via-[#061666] to-[#020b33] bg-opacity-50 backdrop-blur-md shadow-xl w-full absolute top-[70px] left-0 z-50">
+          <ul className="flex flex-col px-6 py-4 space-y-3 text-base font-medium list-none">
             {navLinks.map(({ name, to }) => (
               <li key={to}>
                 <Link
                   to={to}
                   onClick={closeMenu}
-                  className={`block transition ${
-                    pathname === to ? "text-[#00c3ff]" : "text-slate-200 hover:text-[#00c3ff]"
-                  }`}
+                  className={`block transition ${pathname === to ? "text-[#00c3ff]" : "text-slate-200 hover:text-[#00c3ff]"
+                    }`}
                 >
                   {name}
                 </Link>
@@ -144,9 +143,9 @@ function getSVGPath(icon) {
         <path d="M22.675 0H1.325C.593 0 0 .593 0 1.326v21.348C0 23.407.593 24 1.325 24H12.82v-9.294H9.692V11.08h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.464.098 2.795.142v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.312h3.587l-.467 3.626h-3.12V24h6.116C23.407 24 24 23.407 24 22.674V1.326C24 .593 23.407 0 22.675 0z" />
       );
     case "google":
-  return (
-    <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09zM12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23zM5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62zM12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#4285F4"/>
-  );
+      return (
+        <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09zM12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23zM5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62zM12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#4285F4" />
+      );
     default:
       return null;
   }
